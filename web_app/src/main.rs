340f4a23fed649c50deb7b2860289dea7ce77c02
Gabriel Leite Bessa <gabrielleitebessa@gmail.com>
2023-08-10 23:08:29 -0300

    Infra pronta

--- web_app/src/main.rs
+++ web_app/src/main.rs
@@ -1,3 +1,34 @@
+use yew::prelude::*;
+
+struct Counter {
+    value: i64
+}
+
+#[function_component(App)]
+fn app() -> Html {
+    let state = use_state(|| Counter {
+        value: 0
+    });
+
+    let onclick = {
+        let state = state.clone();
+
+        Callback::from(move |_| {
+            state.set(Counter {
+                value: state.value + 1
+            })
+        })
+    };
+
+    html! {
+        <div>
+            <p>{ state.value }</p>
+            <button onclick={onclick}>{ "+1" }</button>
+        </div>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new()
+        .render();
 }
